6d66658cfe1c9b5f372426257871bd5e7409a64f
hueseek <hueseek@gmail.com>
2026-01-16 17:39:17 +0900

    Add text edit te1 and fix btn1 handler function

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import sys
 
 # PyQt5 -> PySide6, 애플리케이션 핸들러와 빈 GUI 위젯
 # QMessageBox : 메시지박스 위젯
-from PySide6.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout, QMessageBox) 
+# QPlainTextEdit 추가
+from PySide6.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout, QMessageBox, QPlainTextEdit) 
 from PySide6.QtGui import QIcon     # icon을 추가하기 위한 라이브러리
 
 class Calculator(QWidget):  # QWidget을 상속받아서 클래스 정의
@@ -14,11 +15,14 @@ class Calculator(QWidget):  # QWidget을 상속받아서 클래스 정의
         self.initUI()       # 나머지 초기화는 initUI 함수에 정의
 
     def initUI(self):
+        self.te1 = QPlainTextEdit() # 텍스트 에디트 위젯 생성
+        self.te1.setReadOnly(True)  # 텍스트 에디트 위젯을 읽기만 가능하도록 수정
+        
         self.btn1 = QPushButton('Message', self)    # 버튼 추가
         self.btn1.clicked.connect(self.activateMessage) # 버튼 클릭 시 핸들러 함수 연결
 
         vbox = QVBoxLayout()    # 수직 레이아웃 위젯 생성
-        vbox.addStretch(1)      # 빈 공간
+        vbox.addWidget(self.te1)    # 수직 레이아웃에 텍스트 에디트 위젯 추가
         vbox.addWidget(self.btn1)   # 버튼 위치
         vbox.addStretch(1)      # 빈 공간
 
@@ -29,8 +33,9 @@ class Calculator(QWidget):  # QWidget을 상속받아서 클래스 정의
         self.resize(256, 256)   # 윈도우 사이즈
         self.show()             # 윈도우를 화면에 표시되도록 호출
     
-    def activateMessage(self):  # 버틍을 클릭할 때 동작하는 함수 : 메시지 박스 출력
-        QMessageBox.information(self, "information", "Button clicked!")
+    def activateMessage(self):  # 핸들러 함수 수정 : 메시지가 텍스트 에디트에 출력되도록
+        # QMessageBox.information(self, "information", "Button clicked!")
+        self.te1.appendPlainText("Button clicked!")
 
 if __name__ == '__main__':      # PySide는 애클리케이션 당 1개의 QApplication이 필요함
     app = QApplication(sys.argv)    # QApplication 인스턴스 생성
