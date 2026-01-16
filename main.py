3bcb999fab77a4eb50abe17fde787c97b9b42cff
hueseek <hueseek@gmail.com>
2026-01-16 17:22:09 +0900

    Add icon.png, .gitignore and push button with handler function in main.py

--- main.py
+++ main.py
@@ -1,18 +1,38 @@
 # ch 4.2.1 main.py
+# 애플리케이션 구현에 필요한 라이브러리 추가
 import sys
-from PySide6.QtWidgets import QApplication, QWidget
-class Calculator(QWidget):
+
+# PyQt5 -> PySide6, 애플리케이션 핸들러와 빈 GUI 위젯
+# QMessageBox : 메시지박스 위젯
+from PySide6.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout, QMessageBox) 
+from PySide6.QtGui import QIcon     # icon을 추가하기 위한 라이브러리
+
+class Calculator(QWidget):  # QWidget을 상속받아서 클래스 정의
 
     def __init__(self):
-        super().__init__()
-        self.initUI()
+        super().__init__()  # 부모 클래스 QWidget 초기화
+        self.initUI()       # 나머지 초기화는 initUI 함수에 정의
 
     def initUI(self):
-        self.setWindowTitle('Calculator')
-        self.resize(256, 256)
-        self.show()
+        self.btn1 = QPushButton('Message', self)    # 버튼 추가
+        self.btn1.clicked.connect(self.activateMessage) # 버튼 클릭 시 핸들러 함수 연결
 
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    view = Calculator()
-    sys.exit(app.exec())
+        vbox = QVBoxLayout()    # 수직 레이아웃 위젯 생성
+        vbox.addStretch(1)      # 빈 공간
+        vbox.addWidget(self.btn1)   # 버튼 위치
+        vbox.addStretch(1)      # 빈 공간
+
+        self.setLayout(vbox)    # 빈 공간 - 버튼 - 빈 공간 순으로 수직 배치된 레이아웃 설정
+
+        self.setWindowTitle('Calculator')   # 윈도우에 표시되는 타이틀
+        # self.setWindowIcon(QIcon('icon.png'))   # 윈도우 아이콘 추가(Mac에서는 기본적으로 아이콘 비활성화)
+        self.resize(256, 256)   # 윈도우 사이즈
+        self.show()             # 윈도우를 화면에 표시되도록 호출
+    
+    def activateMessage(self):  # 버틍을 클릭할 때 동작하는 함수 : 메시지 박스 출력
+        QMessageBox.information(self, "information", "Button clicked!")
+
+if __name__ == '__main__':      # PySide는 애클리케이션 당 1개의 QApplication이 필요함
+    app = QApplication(sys.argv)    # QApplication 인스턴스 생성
+    view = Calculator()             # Calculator 윈도우 인스턴스 생성
+    sys.exit(app.exec())            # 애플리케이션이 이벤트 처리를 하도록 루프 구동
