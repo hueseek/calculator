25c6d012dfa3f97cf5cd039ec8e3af3846f4fc69
justin <jjustinys@gmail.com>
2026-01-18 19:47:49 +0900

    Modify btn1, le1 and emthod name to setDisplay in ui.py

--- ui.py
+++ ui.py
@@ -13,11 +13,13 @@ class View(QWidget):
         self.te1 = QPlainTextEdit()
         self.te1.setReadOnly(True)
 
-        self.btn1 = QPushButton('Message', self)
+        self.btn1 = QPushButton('Calc', self)   # 버튼 이름 변경
         self.btn2 = QPushButton('Clear', self)
 
         self.le1 = QLineEdit('0', self)     # 라인 에디터1 추가
         self.le1.setAlignment(QtCore.Qt.AlignRight) # 라인 에디트1 문자열 배치 설정
+        self.le1.setFocus(True)     # 포커스 설정
+        self.le1.selectAll()        # 텍스트 전체 설정
 
         self.le2 = QLineEdit('0', self)     # 라인 에디터2 추가
         self.le2.setAlignment(QtCore.Qt.AlignRight) # 라인 에디트2 문자열 배치 설정
@@ -48,7 +50,7 @@ class View(QWidget):
         self.resize(256, 256)
         self.show()
 
-    def activateMessage(self):
+    def setDisplay(self):       # 메서드 이름 변경
         self.te1.appendPlainText("Button clicked!")
 
     def clearMessage(self):
