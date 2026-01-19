0cd3e066c103668b5e4f3f9157f1e3350d503b18
hueseek <hueseek@gmail.com>
2026-01-19 23:04:25 +0900

    Modify pow function using exception

--- ctrl.py
+++ ctrl.py
@@ -39,7 +39,10 @@ class Control:
         return a / b
 
     def pow(self, a, b):    # 제곱 함수 수정
-        if (a == 0):
-            return 0
-        else:
-            return pow(a, b)
+        try:
+            if (a == 0):
+                raise Exception("Base Error")
+        except Exception as e:
+            return e    
+            
+        return pow(a, b)
