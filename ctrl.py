299786104ab71de50cc1b33109d64c2ec4b6c738
hueseek <hueseek@gmail.com>
2026-01-19 16:10:02 +0900

    Add power operator to combobox and fix some errors

--- ctrl.py
+++ ctrl.py
@@ -9,7 +9,7 @@ class Control:
         pass
 
     def connectSignals(self):
-        self.view.btn1.clicked.connect(self.view.calculate) # 버튼 1 연결을 변경
+        self.view.btn1.clicked.connect(self.view.setDisplay) # 버튼 1 연결을 변경
         self.view.btn2.clicked.connect(self.view.clearMessage)
 
     def sum(self, a, b):    # 예외 처리 기능 추가
